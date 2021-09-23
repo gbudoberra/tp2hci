218e22e2638d3c9ae09486ad5bfe7cd65adb1319
gbudoberra <gbudoberra@itba.edu.ar>
2021-09-23 19:37:14 -0300

    Segundo Commit

---
+++ src/views/explore.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div>
+    <!-- <h1> Explore en proceso... </h1>
+     <h2> texto de prueba</h2> -->
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-btn
+              depressed
+              elevation="2"
+              outlined
+              plain
+              raised
+              rounded
+              color="blue"
+          >Boton</v-btn>
+        </v-container>
+      </v-main>
+    </v-app>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Explore",
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/myRoutines.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "MyRoutines"
+}
+</script>
+
+<style scoped>
+
+</style>
