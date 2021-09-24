2f223dd7ec7ec55b6e9f37bca9d15721b19230b1
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2021-09-24 18:12:53 -0300

    Comienzo de app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,103 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/explore">Explore</router-link> |
-      <router-link to="/favorites">Favorites</router-link> |
-      <router-link to="/profile">Profile</router-link> |
-      <router-link to="/MyRoutines">MyRoutines</router-link>
-    </div>
-    <router-view/>
-  </div>
+
+  <v-app id="inspire">
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        color="blue lighten-1"
+    >
+
+      <v-list
+          dense
+          nav
+          two-line>
+        <v-list-item>
+          <v-list-item-content>
+
+            <v-list-item-title>
+              <v-icon>explore</v-icon>
+              <router-link to="/explore">Explore</router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-icon>star</v-icon>
+              <router-link to="/favorites">Favorites</router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-icon>person</v-icon>
+              <router-link to="/profile">Profile</router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-icon>fitness_center</v-icon>
+              <router-link to="/MyRoutines">MyRoutines</router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+
+
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app
+    color="blue lighten-4">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-container>
+        <v-row justify="space-between">
+          <v-col align="start" align-self="center">
+            <v-toolbar-title>TrainMe</v-toolbar-title>
+          </v-col>
+
+          <v-col align="end" align-self="center">
+            <v-btn
+                fab
+                text
+
+            ><v-icon size="35px">search</v-icon>
+            </v-btn>
+
+          </v-col>
+
+
+
+        </v-row>
+      </v-container>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({ drawer: null }),
+
 }
 </script>
 
@@ -26,4 +110,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+
+
 </style>
