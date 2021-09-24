4428a08127486dd8ce21364392a50eabdb0ad475
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2021-09-24 20:53:16 -0300

    Cambio colores

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -10,14 +10,14 @@ export default new Vuetify({
         dark: true,
         themes: {
             light: {
-                primary: '#0e6aef',
-                secondary: '#009688',
-                accent: '#f403e0',
+                primary: '#BBDEFB',
+                secondary: '#769FCD',
+                accent: '#000000',
             },
             dark:{
-                primary: '#4159d2',
-                secondary: '#03a9f4',
-                accent: '#00ffff',
+                primary: '#01135e',
+                secondary: '#0372ab',
+                accent: '#ffffff',
             }
         },
     },
