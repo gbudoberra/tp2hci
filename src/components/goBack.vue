38f85b322ce0e242b75855fa84c4ed3addb18846
bsquillari <bsquillari@itba.edu.ar>
2021-09-28 16:50:31 -0300

    Login y goBack files. En proceso.

---
+++ src/components/goBack.vue
@@ -0,0 +1,29 @@
+<template>
+<span class="goBack">
+  <v-btn @click="goBack">Go back</v-btn>
+</span>
+</template>
+
+<script>
+export default {
+  name: "goBack",
+  methods: {
+    goBack(){
+      return this.$router.go(-1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+<!--
+<transition
+name="tipo de transicion"
+>
+
+
+-->
---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+<!--  formulari. Buscar en vuetify o ver clase de gonza al final-->
+</div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style scoped>
+
+</style>
