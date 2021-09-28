c6c0dade005dde3d663fae700296cf62a5d7d555
bsquillari <bsquillari@itba.edu.ar>
2021-09-28 16:51:25 -0300

    Components pasados a funcion. Y cosas comentadas en proceso

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Favorites from "../views/favorites.vue";
-import Profile from "../views/profile.vue";
-import MyRoutines from "../views/myRoutines.vue";
-import Explore from "../views/explore.vue";
+// import Favorites from "../views/favorites.vue";
+// import Profile from "../views/profile.vue";
+// import MyRoutines from "../views/myRoutines.vue";
+// import Explore from "../views/explore.vue";
 
 Vue.use(VueRouter);
 
@@ -11,30 +11,65 @@ const routes = [
   {
     path:"/explore",
     name:"Explore",
-    component: Explore
+    component: () => import( /* webpackChunkName: "explore" */ '../views/explore')
   },
   {
     path: "/profile",
     name:"Profile",
-    component: Profile
+    component: () => import( /* webpackChunkName: "profile" */ '../views/profile')
   },
   {
     path: "/favorites",
     name: "Favorites",
-    component: Favorites
+    component: () => import( /* webpackChunkName: "favs" */ '../views/favorites')
   },
   {
     path: "/MyRoutines",
     name: "MyRoutines",
-    component: MyRoutines
-  }
+    component: () => import( /* webpackChunkName: "myRoutines" */ '../views/myRoutines')
+  },
+  {
+    path: "/details/:id",
+    name: "routineDetails",
+    //meta: requiresAuth
+    props: true,
+    // beforeEnter: (to, from, next) => {
+    //   const exists = store.routine.find(
+    //       routine => routine.id === to.params.id
+    //   )
+    //   if(exists){
+    //     next()
+    //   } else {
+    //     next({name: 'notFound'})
+    //   }
+    // },
+    component: () => import( /* webpackChunkName: "details" */ '../views/routineDetails'),
+  },
+  // {
+    // path: '/notFound'
+  //   alias: '*',
+  //   name: 'notFound',
+  //   component: notFound,
+  // }
 ];
 
 const router = new VueRouter(
     {
-      routes
+      mode: "history",
+      routes,
     }
 );
 
+// router.beforeEach(
+//     (to, from, next) => {
+//       if(to.meta.requiresAuth){
+//         // login
+//       } else {
+//         next()
+//       }
+//     }
+// )
+
 export default router;
+
 
