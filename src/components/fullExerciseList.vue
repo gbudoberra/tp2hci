46625479b6d953345a4c691f443b14dd460d409f
bsquillari <bsquillari@itba.edu.ar>
2021-10-14 20:34:01 -0300

    Coranzon de favs. Y bug fix

--- src/components/fullExerciseList.vue
+++ src/components/fullExerciseList.vue
@@ -8,7 +8,7 @@
           <h1 class="text-h4">MyExercises</h1>
         </v-row>
         <v-row>
-          <page-arrows v-on:nextPage="nextPage" v-on:prevPage="prevPage" :prev-condition="allExercises.exercisePage!==0" :next-condition="allExercises.isRoutinesLast"/>
+          <page-arrows v-on:nextPage="nextPage" v-on:prevPage="prevPage" :prev-condition="allExercises.exercisePage!==0" :next-condition="!allExercises.exerciseLastPage"/>
           <div>
             <pop-up-exercise title="New Exercise" :exercise-already-exists="false">
               <template v-slot:button>
